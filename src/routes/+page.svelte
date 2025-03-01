0a64153a373b0d64f75de3ae6f69be07850d038b
Wenhan Xue <wenhanxue@wellesley.edu>
2025-02-28 20:39:12 -0500

    lab 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Home</title>
+  </svelte:head>
+  
+    <h1>Wenhan Xue</h1>
+    <!-- <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="contact/index.html">Contact</a></li>
+            <li><a href="projects/index.html">Projects</a></li>
+            <li><a href="cv/index.html">CV</a></li>
+            <li><a href="https://github.com/wenhanxue" target="_blank">GitHub</a></li>
+        </ul>
+    </nav> -->
+    <p>hi</p>
+    <img src="image/cat.jpg" alt="picture of my cat in the kitchen staring up into the camera">
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+  <title>Contact me</title>
+</svelte:head>
+
+<h1>Contact</h1>
+<!-- <nav>
+    <ul>
+        <li><a href="../index.html">Home</a></li>
+        <li><a href="index.html">Contact</a></li>
+        <li><a href="../projects/index.html">Projects</a></li>
+        <li><a href="../cv/index.html">CV</a></li>
+        <li><a href="https://github.com/wenhanxue" target="_blank">GitHub</a></li>
+    </ul>
+</nav> -->
+<form action="mailto:wx100@wellesley.edu" method="post" enctype="text/plain">
+    <label>Email: <input name="email" type="email"></label>
+    <label>Subject: <input name="subject"></label>
+    <label>Message: <textarea name="body"></textarea></label>
+    <button>Send</button>
+</form>
