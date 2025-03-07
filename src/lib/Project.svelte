4849d75e92dd561b8cad08b67bb17c5d5bb94aaf
Wenhan Xue <wenhanxue@wellesley.edu>
2025-03-07 17:18:19 -0500

    lab 4 + a4

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -4,24 +4,19 @@
 </script>
 
 <article>
-    <svelte:element this={"h" + hLevel}><a href="assignments/a2/A2_report.html">{data.title}</a></svelte:element>
+    <svelte:element this={"h" + hLevel}><a href={data.url}>{data.title}</a></svelte:element>
     <img src={data.image} alt="">
     <p>{data.description}</p>
 </article>
 
 <style>
-    .projects {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(15em, 1fr));
-}
-
-    .projects article {
+    article {
         grid-template-rows: subgrid;
         grid-row: span 3;
         padding: 0.5em;
     }
 
-    .projects article h3 {
+    article h3 {
         margin: 0;
     }
 </style>
